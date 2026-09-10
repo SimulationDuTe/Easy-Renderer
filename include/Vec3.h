8d14406209009df321a30d3e7b0148f6c2d8bee4
SimulationDuTe <F392749251@outlook.com>
2026-09-10 19:22:39 +0800

    实现了三项光照，渲染出了旋转的明暗立方体

--- include/Vec3.h
+++ include/Vec3.h
@@ -6,14 +6,55 @@ struct Vec3
 	Vec3(float _x = 0, float _y = 0, float _z = 0, float _w = 1) : x(_x), y(_y), z(_z), w(_w) {}
 
 	//标量乘
-	Vec3 operator*(float s) const { return Vec3(x * s, y * s, z * s,1.0f); }
+	Vec3 operator*(float s) const { return Vec3(x * s, y * s, z * s,w); }
 	//加法
-	Vec3 operator+(const Vec3& o) const { return Vec3(x + o.x, y + o.y, z + o.z,1.0f); }
+	Vec3 operator+(const Vec3& o) const { return Vec3(x + o.x, y + o.y, z + o.z,w + o.w); }
+	//减法
+	Vec3 operator-(const Vec3& o) const { return Vec3(x - o.x, y - o.y, z - o.z, w - o.w); }
 
-	//透视除法（将齐次坐标转换为三维坐标）
+
+	//点乘(其实在衡量：两个向量方向有多一致。)
+	//a·b = |a||b|cos(θ)。当a、b都是单位向量时，a·b = cos(θ)。
+	/*
+		小于 90°	方向大致相同 
+		等于 90°	垂直（最常用） 
+		大于 90°	方向大致相反 
+	*/	
+	float dot(const Vec3& o) const { return x * o.x + y * o.y + z * o.z; }
+
+
+	//叉乘
+	/*\vec a * \vec b = (a2*b3 - a3* b2, a3*b1 - a1*b3, a1*b2 - a2*b1)*/
+	//一个平面上，任意两个不共线的向量，叉乘一下，就得到法线
+	Vec3 cross(const Vec3& o) const
+	{
+		return Vec3
+		(
+			y * o.z - z * o.y,
+			z * o.x - x * o.z,
+			x * o.y - y * o.x,
+			1.0f
+		);
+	}
+
+	//归一化
+	//因为需要求单位向量，所以需要归一化
+	//归一化就是把一个向量变成单位向量，也就是长度变成 1，方向不变（向量除以长度）
+	//向量长度 = 分量的平方和开根号
+	Vec3 normalized() const
+	{
+		float lenSq = x * x + y * y + z * z;
+		if (lenSq < 1e-8f) return Vec3(0, 0, 0, 1.0f);
+		float inv = 1.0f / sqrtf(lenSq);
+		return Vec3(x * inv, y * inv, z * inv, 1.0f);//因为除法开销大，先求平方和的倒数，再乘以分量，效率更高
+	}
+
+
+	//透视除法
+	// 变换到NDC坐标[-1,1]
 	void PerspectiveDivision()
 	{
-		if (!w == 0.0f)
+		if (w != 0.0f)
 		{
 			x /= w;
 			y /= w;
--- src/main.cpp
+++ src/main.cpp
@@ -53,6 +53,8 @@ int main(int argc, char* argv[])
 	{-0.5,-0.5,-0.5}, { 0.5,-0.5,-0.5}, { 0.5, 0.5,-0.5}, {-0.5, 0.5,-0.5},  // 后 4 点
 	{-0.5,-0.5, 0.5}, { 0.5,-0.5, 0.5}, { 0.5, 0.5, 0.5}, {-0.5, 0.5, 0.5}   // 前 4 点
 	};
+
+	//创建立方体面索引数组
 	int cubeFaces[12][3] = {   // 6 个面，每个面 2 个三角形
 		{0,1,2},{0,2,3},  // 面0
 		{4,6,5},{4,7,6},  // 面1
@@ -62,12 +64,26 @@ int main(int argc, char* argv[])
 		{4,5,1},{4,1,0},  // 面5
 	};
 
-	//创建顶点颜色数据
-	Vec3 faceColors[6] = {
-	{255,0,0},{0,255,0},{0,0,255},{255,255,0},{255,0,255},{0,255,255}
-	};
+	//方向光：固定方向。
+	Vec3 lightDir = Vec3(0.5f, 1.0f, 1.0f).normalized();//单位向量
+	//物体基础色
+	Vec3 baseColor = Vec3(255, 255, 255); 
+	//摄像机位置：固定在原点(0,0,0)，看向 -Z 方向
+	Vec3 cameraPos = Vec3(2, 2, 2);
+	//高光颜色
+	Vec3 white = Vec3(255, 255, 255);
+	//环境光强度
+	float ambientStrength = 0.1f;
+	//高光强度
+	float specularStrength = 0.8f;
+	//漫反射强度
+	float diffuseStrength = 0.3f;
+	//光泽
+	float shininess = 16.0f;//通过powf，将高光收窄
 
-	float angle = 0.0f; //旋转角度
+
+	//旋转角度
+	float angle = 0.0f; 
 
 	//主循环
 	bool done = false;
@@ -95,7 +111,7 @@ int main(int argc, char* argv[])
 		/*
 		局部坐标 (cubeVerts)
 		↓ 模型矩阵 (旋转)
-		世界坐标
+		世界坐标		← 光照
 		↓ 视图矩阵 (平移 -5)
 		相机坐标
 		↓ 投影矩阵 (透视)
@@ -116,23 +132,57 @@ int main(int argc, char* argv[])
 
 		//投影矩阵.透视投影
 		Matrix4 projection = Matrix4::perspective(60.0f, (float)W / H, 0.1f, 100.0f);
-		
-		//Mvp矩阵（顺序从后到前）
-		Matrix4 mvp = projection * view * model;
 
-		for (int f = 0; f < 12; f++) {
-			Vec3 tri[3], col[3];
-			for (int i = 0; i < 3; i++) {
-				//获取顶点索引
-				int idx = cubeFaces[f][i];
-				Vec3 v = mvp * cubeVerts[idx];     // ① 变换到裁剪空间（w 有意义了）
-				v.PerspectiveDivision();             // ② 透视除法 → NDC
-				v.x = (v.x + 1) * 0.5f * W;        // ③ 视口变换
-				v.y = (1 - v.y) * 0.5f * H;        //    ★ y 要翻转（NDC 上=正，屏幕下=正）
-				v.z = (v.z + 1) * 0.5f;            //    z 从 [-1,1] 映射到 [0,1]
+		Matrix4 projview = projection * view; //先视图再投影（model 单独用，为了拿世界坐标）
+
+		for (int f = 0; f < 12; f++) 
+		{
+			//从模型空间到世界空间的顶点变换，获取世界坐标
+			Vec3 w[3];
+			for (int i = 0; i < 3; i++) 
+				w[i] = model * cubeVerts[cubeFaces[f][i]];
+
+			//面法线
+			/*假设三角形ABC，要求法线，就得先有向量，B-A一个，C-A一个，然后这两个叉乘得到面法线,最后归一化为单位向量*/
+			Vec3 normal = (w[2] - w[0]).cross(w[1] - w[0]).normalized();
+
+			//面中心
+			//三角形三个顶点的平均值，代表这个面的中心位置
+			Vec3 faceCenter = (w[0] + w[1] + w[2]) * (1.0f / 3.0f);
+
+
+			//计算从面中心到相机的方向向量，并归一化为单位向量
+			Vec3 viewDir = (cameraPos - faceCenter).normalized();
+			//计算半程向量，并归一化为单位向量
+			Vec3 halfDir = (viewDir + lightDir).normalized();//半程向量 = 指向光源方向 + 指向视线方向，归一化
+			
+			/*三项光照*/
+			//一：环境光：环境光强度 * 物体基础色
+			Vec3 ambient = ambientStrength * baseColor;
+			//二：漫反射：漫反射强度 * 物体基础色 * max(0, 法线·光源方向)
+			Vec3 diffuse = diffuseStrength * baseColor * std::max(0.0f, normal.dot(lightDir));
+			//三：镜面反射：镜面反射强度 * 高光颜色 * pow(max(0, 法线·半程向量), 光泽),钳制到0~1；
+			float spec = std::powf(std::max(0.0f,normal.dot(halfDir)), shininess);
+			Vec3 specular = specularStrength * white * spec;
+
+			//最终颜色 = 环境光 + 漫反射 + 镜面反射
+			Vec3 litColor = ambient + diffuse + specular;
+	
+			
+			//变换到裁剪空间 → 透视除法 → 视口
+			Vec3 tri[3];
+			for (int i = 0; i < 3; i++)
+			{
+				Vec3 v = projview * w[i];
+				v.PerspectiveDivision(); //透视除法 变换到NDC坐标[-1,1]
+				//视口变换：NDC [-1, 1] → 屏幕坐标 [0, W] 和 [0, H]
+				v.x = (v.x + 1) * 0.5f * W;
+				v.y = (1 - v.y) * 0.5f * H;
+				v.z = (v.z + 1) * 0.5f;
 				tri[i] = v;
-				col[i] = faceColors[f / 2];        // 每面一种颜色
 			}
+			//三个顶点同一个颜色 = Flat（逐面）着色
+			Vec3 col[3] = { litColor, litColor, litColor };
 			Rasterizer::DrawTriangle(framebuffer, zbuffer, fmt, tri, col);
 		}
 
